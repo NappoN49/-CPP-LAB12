8f17c05c5ff10b33a658599373fa69e422a0017a
NappoN49 <parawin39228@gmail.com>
2025-12-30 15:04:27 +0700

    lab12_4 complete

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template<typename alpha>
+void mySwap(alpha &,alpha &); 
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +33,9 @@ int main(){
 	return 0;
 }
 
+template<typename beta>
+void mySwap(beta &x,beta &y){
+    beta temp=x;
+    x=y;
+    y=temp;
+}
